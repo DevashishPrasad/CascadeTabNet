b07f4fcfc0b8c741cb08a6dc9663a364d90f8c11
Manish Dnyaneshwar Visave <43295558+ManishDV@users.noreply.github.com>
2020-04-26 20:41:28 +0530

    Add files via upload

---
+++ Evaluations/Tablebank/evaluation.py
@@ -0,0 +1,269 @@
+# %cd "/content"
+%cd "/content/drive/My Drive/Mmdetection/mmdetection"
+
+from collections import namedtuple
+import numpy as np
+import cv2
+import glob
+
+import time
+import matplotlib
+import matplotlib.pylab as plt
+import glob
+import mmcv
+from mmcv.runner import load_checkpoint
+import mmcv.visualization.image as mmcv_image
+import lxml.etree as etree
+import numpy as np
+import os
+import subprocess
+import sys
+from mmdet.models import build_detector
+from mmdet.apis import inference_detector, show_result, init_detector
+import cv2
+import json
+
+
+#Function to implement line detection.
+def line_detection(image):
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 1)
+    bw = cv2.bitwise_not(bw)
+    horizontal = bw.copy()
+    vertical = bw.copy()
+
+    # [horiz]
+    # Create structure element for extracting horizontal lines through morphology operations
+    horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 1))
+
+    # Apply morphology operations
+    horizontal = cv2.erode(horizontal, horizontalStructure)
+    horizontal = cv2.dilate(horizontal, horizontalStructure)
+
+    horizontal = cv2.dilate(horizontal, (1,1), iterations=5)
+    horizontal = cv2.erode(horizontal, (1,1), iterations=5)
+
+    # cv2.imshow("hori",horizontal)
+    # cv2.waitKey(0)
+    hor_lines = cv2.HoughLinesP(horizontal,rho=1,theta=np.pi/180,threshold=100,minLineLength=150,maxLineGap=10)
+    if hor_lines is None:
+        return None,None
+    temp_line = []
+    for line in hor_lines:
+        for x1,y1,x2,y2 in line:
+            temp_line.append([x1,y1-5,x2,y2-5])
+
+    hor_lines = sorted(temp_line,key=lambda x: x[1])
+    print(len(hor_lines))
+    for x1, y1, x2, y2 in hor_lines:
+        cv2.line(image, (x1,y1-5), (x2,y2-5), (0, 255, 0), 1)
+
+    
+
+    # cv2.imshow("image",image)
+    # cv2.waitKey(0)
+    
+    # [vert]
+    # Create structure element for extracting vertical lines through morphology operations
+    verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 15))
+
+    # Apply morphology operations
+    vertical = cv2.erode(vertical, verticalStructure)
+    vertical = cv2.dilate(vertical, verticalStructure)
+
+    vertical = cv2.dilate(vertical, (1,1), iterations=5)
+    vertical = cv2.erode(vertical, (1,1), iterations=5)
+
+    ver_lines = cv2.HoughLinesP(vertical,rho=1,theta=np.pi/180,threshold=110,minLineLength=150,maxLineGap=10)
+    if ver_lines is None:
+        return None,None
+    temp_line = []
+    for line in ver_lines:
+        for x1,y1,x2,y2 in line:
+            temp_line.append([x1,y1-5,x2,y2-5])
+
+    ver_lines = sorted(temp_line,key=lambda x: x[0])
+    print(image.shape)
+    for x1, y1, x2, y2 in ver_lines:
+        cv2.line(image, (x1,y1-5), (x2,y2-5), (0, 255, 0), 2)
+    return hor_lines,ver_lines
+
+
+#Function to calculate IoU between two boxes
+def bb_intersection_over_union(ground_truth, detection):
+	global tablecount
+	# determine the (x, y)-coordinates of the intersection rectangle
+	tablecount+=1	
+	xA = max(ground_truth[0], detection[0])
+	yA = max(ground_truth[1], detection[1])
+	xB = min(ground_truth[2], detection[2])
+	yB = min(ground_truth[3], detection[3])
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	gtArea = (ground_truth[2] - ground_truth[0] + 1) * (ground_truth[3] - ground_truth[1] + 1)
+	detectionArea = (detection[2] - detection[0] + 1) * (detection[3] - detection[1] + 1)
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	#Calculate iou , precision and recall and append precision and recall to global lists
+	iou = interArea / float(gtArea + detectionArea - interArea)
+	precision_iou = interArea / float(detectionArea)
+	recall_iou = interArea / float(gtArea)
+	precision.append(precision_iou)
+	recall.append(recall_iou)
+
+	# return the intersection over union value
+	return iou
+
+#List of epochs to be evaluated
+epochs = ['epoch_22.pth','epoch_23.pth','epoch_24.pth','epoch_25.pth','epoch_26.pth']
+# Testing Images
+# img_files = glob.glob("/content/drive/My Drive/TableBank/tablebank_both_augment/*.*")
+
+
+#test json for word subset images . If you are using latex test subset then provide path for respective test json file
+with open('/content/drive/My Drive/TableBank/word_test.json') as f:
+	data = json.load(f)
+
+#Iterating over the list of epochs
+for epoch in epochs:
+	tablecount = 0
+	precision =[]
+	recall = []
+	try:
+		#cofig file path and checkpoint file path provide appropriate paths
+		score_thr = 0.9
+		config_fname = "/content/drive/My Drive/cascade_mask_rcnn_hrnetv2p_w32_20e.py"
+		checkpoint_file_path = "/content/drive/My Drive/Mmdetection/word_cascade_mask_rcnn_hrnetv2p_w32_20e/"+epoch
+
+		# build the model from a config file and a checkpoint file
+		model = init_detector(config_fname, checkpoint_file_path)
+		#{dict_keys(['info', 'licenses', 'images', 'categories', 'annotations'])}
+		#{'file_name': '1401.0007_15.jpg', 'id': 1, 'license': 1, 'width': 596, 'height': 842}
+		#{'segmentation': [[85, 396, 85, 495, 510, 495, 510, 396]], 'area': 42075, 'image_id': 1, 'category_id': 1, 'id': 1, 'iscrowd': 0, 'bbox': [85, 396, 425, 99]}
+
+		gt_boxes = []
+		idx = 0
+		t=0
+		for i in data['images']:
+			# print(i)
+			idx+=1        
+			# if(idx <= 500):
+			# 	continue
+			# if(idx > 1502):
+			# 	break
+			image_name = "/content/drive/My Drive/TableBank/tablebank_word/"+str(i['file_name'])
+			iii = cv2.imread(image_name)
+			#If image is invalid then continue
+			if(iii is None):
+				print("continued")
+				continue
+			#If image is valid then get prediction from model	
+			result = inference_detector(model, image_name)
+			#If no table detected then continue to next image
+			if(len(result[0][0])==0):
+				print("continued again")
+				continue
+
+			#Detecting horizontal and vertical lines in table image 	
+			hor,ver = line_detection(iii)
+
+			# LINE CORRECTION
+			if(hor is not None and ver is not None):    
+				for r in result[0][0]:
+					xmin = 9999
+					ymin = 9999
+					xmax = 9999
+					ymax = 9999
+					for h in hor:
+						# print(h)
+						diff1 = abs(h[1] - r[1])
+						if(diff1 < abs(r[1]-ymin) and abs(h[0]-r[0])<25 and abs(h[2]-r[2])<25):
+							ymin = h[1]
+						diff2 = abs(h[3] - r[3])
+						if(diff2 < abs(r[3]-ymax) and abs(h[0]-r[0])<25 and abs(h[2]-r[2])<25):
+							ymax = h[3]    
+					if(ymin != 9999):      
+						r[1] = ymin
+					if(ymax != 9999):      
+						r[3] = ymax
+					for v in ver:
+						diff1 = abs(v[0] - r[0])
+						if(diff1 < abs(r[0]-xmin) and abs(h[1]-r[1])<25 and abs(h[3]-r[3])<25):
+							xmin = v[0]
+						diff2 = abs(v[2] - r[2])
+						if(diff2 < abs(r[2]-xmax) and abs(h[1]-r[1])<25 and abs(h[3]-r[3])<25):
+							xmax = v[2]       
+					if(xmin != 9999):        
+						r[0] = xmin
+					if(xmax != 9999):        
+						r[2] = xmax    
+
+				# BACK TO NORMAL 
+
+			d_bboxes = np.vstack(result)
+			#Getting ground truth for tables in image
+			for j in data['annotations']:
+				if(j['image_id'] == i['id']):
+					for k in j['segmentation']:
+						gt_boxes.append(k)
+
+			print(i['id'],' ',idx,' ',i['file_name'])
+			# print(gt_boxes)
+			# print(d_bboxes[0][0])
+			# print("Image : {}".format(i))
+			#Calculating minimum distance ground truth from prediction in case of multiple tables in the image
+			for bbox1 in d_bboxes[0][0]:
+				min_dist = 99999
+				min_el = gt_boxes[0][0]
+				for bbox2 in gt_boxes:
+					dist = np.linalg.norm((bbox1[0]-bbox2[0],bbox1[1]-bbox2[1]))
+					if(min_dist > dist):
+						min_dist = dist
+						min_el = bbox2
+				bbox_int1 = bbox1[:-1]
+		
+				# img = cv2.imread(image_name)
+				# cv2.rectangle(img,(bbox_int1[0],bbox_int1[1]),(bbox_int1[2],bbox_int1[3]),(0,255,0))
+				# cv2.imwrite(str(t)+".jpg",img)
+				t = t + 1
+				bbox_int1 = bbox1.astype(np.int32)
+				bbox_int2 = [int(min_el[0]),int(min_el[1]),int(min_el[4]),int(min_el[5])]
+				# bbox_int2 = bbox_int2.astype(np.int32)
+				#Calculating IoU of ground truth and predicted region
+				iou = bb_intersection_over_union(bbox_int1,bbox_int2)
+				print(" IOU : ",iou)
+
+			gt_boxes = []		
+
+	except Exception as e:
+		print("ERROR : ",str(e))
+
+	# global tablecount
+
+	#Sum over all the precision and recall of every table in all images
+	sum_precision = sum(precision)
+	sum_recall = sum(recall)
+
+	#Calculating average precision and recall
+	avg_precision = sum_precision / tablecount
+	avg_recall = sum_recall / tablecount
+
+	#Calculating F1 score
+	f1_score = 2 * ((avg_precision * avg_recall) / (avg_precision + avg_recall))
+
+
+	print("====================================================")
+	print("Precision : ",avg_precision)
+	print("Recall : ",avg_recall)
+	print("F1 : ",f1_score)
+	
+	#Saving the results in file
+	saver = "\n {} {} {}".format(avg_precision,avg_recall,f1_score)
+	print(saver)
+	my_file = open("/content/drive/My Drive/againwordfile.txt", "a")
+	my_file.write(saver)
+	my_file.close()
